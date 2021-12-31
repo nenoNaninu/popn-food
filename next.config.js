8decc4d765f55c05770c793a99f2485aa682cff8
nenoNaninu <neno@melonpan.pro>
2021-12-31 22:44:11 +0900

    comment out

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-//   basePath: '/mesh-viewer'
+    reactStrictMode: true,
+    // basePath: '/mesh-viewer/'
 }
